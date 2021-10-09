5293f8a22d73cfe2d9128fb19438c7157945b774
damanpreet singh <dmnprt4@gmail.com>
2021-10-09 18:29:36 +0530

    feat: Coded Ai to play with User Coded a ai module that will make a move everytime the player moves
    ADDED: ai module
    Will check all the possible positions and then selects one of them
    and places its sign

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
+/**
+ * Author: damanpreet singh
+ * Dated: Oct 9, 2021
+ * 
+ */
+
+
+/**
+ * Object `GameBoard`
+ * makes the functionalities of the board
+ * check for wins and draw
+ * chcecks if the space is availble
+ */
 const GameBoard = (function () {
 
+    // make a new array to store the player signs
     let board = new Array();
 
-    // will make the board
+    // will fill the board
+    // `.` means the space is empty
     const makeBoard = function () {
         board[0] = "_";
         for (let i = 1; i < 10; i++) {
@@ -11,6 +26,7 @@ const GameBoard = (function () {
         return board;
     };
 
+    // checks if the there is any space in the board;
     const isNotFull = function () {
         for (let i = 1; i < 10; i++) {
             if (board[i] === ".") {
@@ -20,7 +36,7 @@ const GameBoard = (function () {
         return false;
     };
 
-
+    // checks if the current spot is available
     const isSpotAvailable = function (spot) {
         if (board[spot] === ".") {
             return true;
@@ -28,13 +44,15 @@ const GameBoard = (function () {
         return false;
     };
 
-
+    // places the sign in the array
     const placeSign = function (sign, spot) {
         if (isSpotAvailable(spot)) {
             board[spot] = sign;
         }
-    }
+    };
 
+    // check if anyone has won or not
+    // checks the rows, cols and the diagonals
     const checkWinner = function (sign) {
         if ((board[1] === sign && board[2] === sign && board[3] === sign) ||
             (board[4] === sign && board[5] === sign && board[6] === sign) ||
@@ -47,32 +65,54 @@ const GameBoard = (function () {
             return true;
         }
         return false;
-    }
+    };
 
+    // give emptySpaces
+    const emptySpaces = function(){
+        let arr = new Array();
+        for(let i = 1; i < 10; i++){
+            if(board[i] === "."){
+                arr.push(i);
+            }
+        }
+
+        return arr;
+    };
+
+    // if board is full and nobody has won
     const draw = function () {
         if (!isNotFull() && !checkWinner('x') && !checkWinner('o')) {
             return true;
         }
         return false;
-    }
+    };
 
 
-    return { makeBoard, isNotFull, placeSign, checkWinner, draw, board };
+    // returns the public methods
+    return { makeBoard, isNotFull, placeSign, checkWinner, draw, emptySpaces, isSpotAvailable};
 })();
 
-
+/**
+ * `Player` object
+ * @param {char} sg 
+ * @param {boolean} status 
+ * creates sign and turn status
+ */
 const Player = function (sg, status = false) {
     let sign = sg;
     let isActive = status;
 
+    // returns the sign of the player
     const getSign = function () {
         return sign;
     }
 
+    // returns if it is the players turn or not
     const getStatus = function () {
         return isActive;
     }
     
+    // toggles the turn
     const changeTurn = function () {
         isActive = !isActive;
     }
@@ -81,56 +121,117 @@ const Player = function (sg, status = false) {
 };
 
 
+// for computer to play
+const ai = (function(){
+
+    let arr;
+
+    // get all the possible moves available at the given time
+    const getEmptySpaces = function(){
+        arr = b.emptySpaces();
+    };
+
+    // Choose a spot to Place the sign
+    const makeMove = function(){
+        getEmptySpaces();
+        let sign = player2.getSign();
+        let spot = arr[Math.floor(Math.random() * arr.length)];
+        b.placeSign(sign, spot);
+        const target = document.getElementById(`${spot}`);
+        if(target){
+            if(!target.classList.contains(player1.getSign())){
+                target.classList.add(sign);
+            }
+        }
+
+        // toggle each other's turn
+        changeStatus();
+    }
+
+    return {makeMove};
+})();
+
+
+// grab the board
 const gameGrid = document.querySelector(".board");
 
+// to listen to user click events
 gameGrid.addEventListener("click", (e) => {
+    
+    // check if the target on which the event is fired is a cell or not
     if(e.target.classList.contains('cell')){
+        // grab the target id
+        let spot = Number(e.target.id);
 
-        if (b.isNotFull() && !b.checkWinner('x') && !b.checkWinner('o') && !b.draw()) {
-            let spot = Number(e.target.id);
+        /**
+         * things to check:
+         * 1. is the board full
+         * 2. have anyone won (x or o)
+         * 3. is it a draw
+         * 4. is the spot available
+         */
+        if (b.isNotFull() && !b.checkWinner('x') && !b.checkWinner('o') && !b.draw() && b.isSpotAvailable(spot)) {
             if (player1.getStatus()) {
-                b.placeSign(player1.getSign(), spot);
+                let sign = player1.getSign();
+                b.placeSign(sign, spot);
+                
                 if(!e.target.classList.contains(player2.getSign())){
-                    e.target.classList.add(player1.getSign());
-                }
-            } else {
-                b.placeSign(player2.getSign(), spot);
-                if(!e.target.classList.contains(player1.getSign())){
-                    e.target.classList.add(player2.getSign());
+                    e.target.classList.add(sign);
                 }
             }
             
-            player1.changeTurn();
-            player2.changeTurn();
             
-            const status = document.getElementById('sign');
+            // change their active status;
+            changeStatus();
+
+        }
+        
+        // game is not over yet
+        let notOver = true;
+        if(b.checkWinner('x')){
+            gameOver('X wins');
+            notOver = false;
+        }else if(b.checkWinner('o')){
+            gameOver('O wins');
+            notOver = false;
+        }else if(b.draw()){
+            gameOver(`It's a Tie`);
+            notOver = false;
+        }
+
+        // computer only makes a move when its his turn and game is not over
+        if(player2.getStatus() && notOver){
+            // added a timeout to add a little delay in Ai's move :)
+            setTimeout(ai.makeMove , 500);
+        }
+    }
+});
+
+
+function changeStatus(){
+    player1.changeTurn();
+    player2.changeTurn();
+
+    const status = document.getElementById('sign');
+    if(status){
         status.textContent = player1.getStatus() ? player1.getSign().toUpperCase() : player2.getSign().toUpperCase();
     }
-    
-    if(b.checkWinner('x')){
-        const status = document.querySelector('.status');
-        status.classList.add('won');
-        status.innerHTML = "";
-        status.textContent = `X wins`;
-    }else if(b.checkWinner('o')){
-        const status = document.querySelector('.status');
-        status.classList.add('won');
-        status.innerHTML = "";
-        status.textContent = `O wins`;
-    }else if(b.draw()){
-        const status = document.querySelector('.status');
-        status.classList.add('won');
-        status.innerHTML = "";
-        status.textContent = `It's a Tie`;
-    }
 }
-});
+
+function gameOver(str){
+    const status = document.querySelector('.status');
+    status.classList.add('won');
+    status.innerHTML = "";
+    status.textContent = str;
+}
 
 
 const restartButton = document.querySelector('.restart');
 
 restartButton.addEventListener('click', startGame);
 
+// restrats the game from the start
+// clears all the existing cells
 function startGame(){
     b = Object.create(GameBoard);
     b.makeBoard();
@@ -148,10 +249,12 @@ function startGame(){
 }
 
 function makeCells(board){
+    // remove all the existing cells
     while(board.firstChild){
         board.removeChild(board.firstChild)
     }
 
+    // insert new ones
     const div = document.createElement('div');
     div.setAttribute('class', 'cell');
     for(let i = 1; i < 10; i++){
